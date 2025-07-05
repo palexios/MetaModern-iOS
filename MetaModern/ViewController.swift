72ae5d9260776f6fe635034888589f78de056dd8
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-07-05 15:32:58 +0300

    Initial Commit

---
+++ MetaModern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetaModern
+//
+//  Created by Александр Павлицкий on 05.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
