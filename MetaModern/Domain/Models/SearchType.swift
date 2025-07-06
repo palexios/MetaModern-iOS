d34406482d1842727d9b733e13a0826f49958645
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-07-06 18:20:18 +0300

    Add SearchType enum

---
+++ MetaModern/Domain/Models/SearchType.swift
@@ -0,0 +1,14 @@
+//
+//  SearchType.swift
+//  MetaModern
+//
+//  Created by Александр Павлицкий on 05.07.2025.
+//
+
+import Foundation
+
+enum SearchType {
+    case author
+    case frame
+    case card
+}
