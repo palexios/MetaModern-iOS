ea907dda4356828ee28b49d32dc5b3d56b44aa0f
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-07-09 17:13:58 +0300

    Add CatalogSearchTextFieldLayout

---
+++ MetaModern/Modules/Catalog/Model/CatalogSearchTextFieldLayot.swift
@@ -0,0 +1,15 @@
+//
+//  CatalogSearchTextFieldLayot.swift
+//  MetaModern
+//
+//  Created by Александр Павлицкий on 07.07.2025.
+//
+
+import Foundation
+
+extension DSLayout {
+    enum CatalogSearchTextFieldLayot {
+        static let heightRatio = DSLayout.SecondaryHeaderViewLayout.heightRatio
+        static let widthRatio = heightRatio
+    }
+}
